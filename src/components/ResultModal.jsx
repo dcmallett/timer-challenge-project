440d3c6082242ad2a62c4d9f9b58bcd19d0e5919
dcmallett <dcmallett28@gmail.com>
2024-08-09 16:12:10 +0100

    added setIntervals to timers

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,12 +1,31 @@
-import { forwardRef } from "react"
+import { forwardRef, useImperativeHandle, useRef } from "react"
 
-const ResultModal = forwardRef(function ResultModal({result, targetTime}, ref) {
+const ResultModal = forwardRef(function ResultModal({targetTime, remainingTime, onReset}, ref) {
+
+  const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2)
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100)
+
+
+  //this is used to define properties and methods that can be used outside of this component
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      }
+    }
+  })
+
+
   return (
-    <dialog className="result-modal" ref={ref}>
-        <h2>You {result}</h2>
-        <p>The target time was <storng>{targetTime}</storng></p>
-        <p>You stopped the timer with <strong>X seconds left</strong></p>
-        <form method="dialog">
+    <dialog className="result-modal" ref={dialog}>
+        {userLost && <h2>You lost</h2>}
+        {!userLost && <h2>Your Score is: {score}</h2>}
+        <p>The target time was <storng>{targetTime} seconds</storng></p>
+        <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left</strong></p>
+        <form method="dialog" onSubmit={onReset}>
             <button>Close</button>
         </form>
     </dialog>
